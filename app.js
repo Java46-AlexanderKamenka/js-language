ded4d90d425fa1dd2a30a4938e19688e3fd075cb
Alex Kamenka <kamenka993@gmail.com>
2022-01-22 04:32:58 +0200

    HW_13 Array(foreach, map)

--- app.js
+++ app.js
@@ -1,42 +1,23 @@
+function myForEach(array, callbackFn){
 
-const arHw = [13, 28, 4, 15, 25, -10, 40, 17,27];
-arHw.sort((a, b) => 
-a % 2 == b % 2 ?
- (a - b) * (a % 2 ? -1 : 1)
- : a % 2 - b % 2);
-
-console.log(arHw);
-
-let matrix = [[1,2], [3,4], [5,6]];
-function matrixTransp(matrix) {
-let result = [];
-result.push([])
-result.push([])
-for(let i = 0; i < matrix.length; i++)
-{
-	for(let j = 0; j < matrix[i].length; j++)
-  {
-    if (j==0)
-    {
-      result[0].push(matrix[i][j]);
-    }
-    else
-    {
-      result[1].push(matrix[i][j]);
-    }
-    
-	}
-}
-console.log(result);
-return result;
-}
-
-matrixTransp(matrix);
-
-
-
-
-
+	for(let i = 0; i < array.length; i++) {
+	  let element = array[i];
   
+	  callbackFn(element, i, array);
+	}
+  }
 
-
+  function myMap(element, callbackFn) {
+	let result = [];
+  
+	myForEach(array, function (element, i, array) {
+	result.push(callbackFn(element, i, array)); });
+  
+	return result;
+  }
+//TEST FUNCTIONALITY
+array = [1,2,3,4,5,6,7];
+array2 =[];
+myForEach(array,(element) => console.log(element));
+array2 = myMap(array, (element) => element ** 2);
+console.log(array2);
