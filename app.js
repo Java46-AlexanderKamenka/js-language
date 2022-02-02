6077d3fc7d0c85f9cd58c24c6e5a9d13087ecae8
Alex Kamenka <kamenka993@gmail.com>
2022-02-02 09:59:36 +0200

    HW-16

--- app.js
+++ app.js
@@ -1,99 +1,67 @@
-function myFilter(array, callbackFn){
- 
-let array_new = [];                         
+/*******************************************HW #16 definition task1 */
+function Deferred() {
+	this.callbacks = []	
+  }
+  Deferred.prototype.then = function(callback) { 
+	this.callbacks.push(callback)
+  }
+
+  Deferred.prototype.resolve = function(value) {
+	let functions = this.callbacks.shift() 
+	if (functions != undefined) {
+
+	  let returns = functions(value)
+
+	  if (returns instanceof Deferred){
+
+	  returns.then(function(value) {
+		  this.resolve(value)
+		})
+	  }
+
+	  else this.resolve(returns)
+	}
+  }
+  
+const d = new Deferred()
+d.then(function(res){ console.log('1 ', res); return 'a'; });
+
+d.then(function(res){ console.log('2 ', res); return 'b'; });
+
+
+d.then(function(res){ console.log('3 ', res); return 'c'; });
+d.resolve('hello');
+
+/*******************************************HW #16 definition task2 */
+
+function MyArray(param) {
+	this.array = [];
+	this.param = param;
 	
-for(let i=0; i<array.length; i++)
-{
-	let element = array[i];
-	if(callbackFn(element, i, array) == true)        
+	MyArray.prototype.set = function (index, value) 
 	{
-	array_new.push(array[i]);                 
-	} 
-}
-	
-	return array_new;                          
-}
-
-function myReduce(array, callbackFn, initialResult) {
-	let i
-	if (initialResult == null){
-		initialResult = array[0];
-		i=1;
+		this.array[index] = value;
 	}
-	else
-	{
-		i=0;
-	}
-	result = initialResult;
-	                            
-	for(i; i<array.length; i++)     
-	{
-		let element = array[i];
-		callbackFn(initialResult, element, i, array)
-		result = result + array[i];
-	}
-		  
-		return result;                              
-	}
-
-
-
-   array = [0,1,2,3,4,5,6,7,8,9];
-   console.log(myFilter(array, a=> a > 5));
-   console.log(myReduce(array, (res, cur) => res + cur, 100));
-
-//TODO write function getPersonsCity(persons, city) , taking array of the persons and a city (see the branch js-objects ) and returning array of the persons living in the given city. For example, find persons living in Rehovot
-function createPerson(id, name, address) {
-	return {id, name, address};
-}
-function createAddress(city, street) {
-	//{city: city, street: street} <=> {city, street}
-	return {city, street}
-}
-const persons = [
-	createPerson(123, "Vasya", createAddress("Rehovot","Parshani")),
-	createPerson(124, "Olya", createAddress("Rehovot","Pr. Plaut")),
-	createPerson(125, "Tolya", createAddress("Tel-Aviv","Dizengoff")),
-	createPerson(126, "Sara", createAddress('Lod', 'Sokolov'))
-]
-
-function getPersonsCity(persons, your_city)
-{
-	let ar = []
-	for(let i = 0; i < persons.length; i++)
-	{
-		if(persons[i].address.city == your_city)
+	MyArray.prototype.get = function (index) {
+		if (this.array[index] == undefined)
 		{
-			ar.push(JSON.stringify(persons[i]));
-		}
-	}
-    return ar;		
-}
-console.log(getPersonsCity(persons, "Rehovot"));
-
-const persons2 = [
-	createPerson(123, "Vasya", createAddress("Rehovot","Parshani")),
-	createPerson(124, "Olya", createAddress("Rehovot","Pr. Plaut")),
-	createPerson(125, "Tolya", createAddress("Tel-Aviv","Dizengoff")),
-	createPerson(126, "Sara", createAddress('Lod', 'Sokolov'))
-]
-
-function movePersonsNoCityAtBeginning(persons, your_city)
-{
-	let ar = []
-	for(let i = 0; i < persons.length; i++)
-	{
-		if(persons[i].address.city != your_city)
-		{
-			ar.unshift(JSON.stringify(persons[i]));
-		}
+			
+			this.array[index] = this.param;
+			return this.array[index];
+		}	 
 		else
-		{
-			ar.push(JSON.stringify(persons[i]));
-		}
+		return this.array[index];
 	}
-    return ar;
+	MyArray.prototype.SetValue = function (value) {
+		
+		this.param = value;
+	}
 }
-console.log(movePersonsNoCityAtBeginning(persons2, "Rehovot"));
+const myArray = new MyArray(10);
+console.log(myArray.get(3727840));
+myArray.set(100,100);
+console.log(myArray.get(100));
+myArray.SetValue(999);
+console.log(myArray.get(87536));
 
 
