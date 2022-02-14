92800b390be8d065b0e76672d735eb182158aa3a
Alex Kamenka <kamenka993@gmail.com>
2022-02-14 03:59:22 +0200

    HW_19

--- app.js
+++ app.js
@@ -1,67 +1,28 @@
-/*******************************************HW #16 definition task1 */
-function Deferred() {
-	this.callbacks = []	
-  }
-  Deferred.prototype.then = function(callback) { 
-	this.callbacks.push(callback)
-  }
+/*      #HW task 1      */
+function getRandomNumber(min, max) {
+    if(min>max)
+    {
+        max = [min,min = max][0];
+        //console.log(`max=${max}, min=${min}`);
+    }
 
-  Deferred.prototype.resolve = function(value) {
-	let functions = this.callbacks.shift() 
-	if (functions != undefined) {
-
-	  let returns = functions(value)
-
-	  if (returns instanceof Deferred){
-
-	  returns.then(function(value) {
-		  this.resolve(value)
-		})
-	  }
-
-	  else this.resolve(returns)
-	}
-  }
-  
-const d = new Deferred()
-d.then(function(res){ console.log('1 ', res); return 'a'; });
-
-d.then(function(res){ console.log('2 ', res); return 'b'; });
-
-
-d.then(function(res){ console.log('3 ', res); return 'c'; });
-d.resolve('hello');
-
-/*******************************************HW #16 definition task2 */
-
-function MyArray(param) {
-	this.array = [];
-	this.param = param;
-	
-	MyArray.prototype.set = function (index, value) 
-	{
-		this.array[index] = value;
-	}
-	MyArray.prototype.get = function (index) {
-		if (this.array[index] == undefined)
-		{
-			
-			this.array[index] = this.param;
-			return this.array[index];
-		}	 
-		else
-		return this.array[index];
-	}
-	MyArray.prototype.SetValue = function (value) {
-		
-		this.param = value;
-	}
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-const myArray = new MyArray(10);
-console.log(myArray.get(3727840));
-myArray.set(100,100);
-console.log(myArray.get(100));
-myArray.SetValue(999);
-console.log(myArray.get(87536));
+console.log(getRandomNumber(10,1));
+/*      #HW task 2      */
+function concatinate(prefix) {
+      
+    return function(prefix2)
+    {
+       // console.log(prefix);
+       // console.log(prefix2);
+        return prefix2 = prefix.concat(prefix2);
+    }     
+}
+const concatApp = concatinate('App - ');
+const concatMessage = concatApp('Test status: Done');
+console.log(concatMessage); 
 
-
+//there should be displayed out App - Test status: Done
